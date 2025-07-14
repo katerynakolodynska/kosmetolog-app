fa871b4ec19762cd7bf940f3ceca15731d0a6aac
katerynakolodynska <kolodinkata@gmail.com>
2025-07-14 22:55:23 +0200

    style

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './i18n.js';
+import './styles/global.css';
 
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
